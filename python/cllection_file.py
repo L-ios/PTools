2df87e2f8b724532882b679378050ba6bae54669
Lionseun Ling <lionseun@gmail.com>
2016-12-27 00:34:05 +0800

    file compare script first commit

--- python/cllection_file.py
+++ python/cllection_file.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import  xlsxwriter
+import xlsxwriter
+
+'''
+主要是用于查找相同的文件
+收集大小，然后获取sha-1 md5.
+'''
+
+
+def main():
+    workbook = xlsxwriter.Workbook('myexcel.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.write('A1', 1)
+    workbook.close()
+
+
+if __name__ == '__main__':
+    main()
